6dd82cb10a7c484a3b8c25fd1e9cdb86c5d1c492
Vadym Volin <vadym.volin@3ss.tv>
2021-10-16 19:00:41 +0300

    ASC-1: Start working with bot api

--- server.py
+++ server.py
@@ -1,27 +1,76 @@
-import telebot
-import PIL
-from PIL import Image
-from requests import get
+"""
+Simple Bot to reply to Telegram messages taken from the python-telegram-bot examples.
+Deployed using heroku.
+Author: liuhh02 https://medium.com/@liuhh02
+"""
 
-#Сюда подставляете свой токен
-bot = telebot.TeleBot('2059855205:AAEX2RnnkBQWnLHxw3PMx5JcYaZhGGvE_9E')
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import os
+PORT = int(os.environ.get('PORT', 5000))
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Пришли мне Emoji, который необходимо сделать размытым.')
-                     
-@bot.message_handler(content_types=['text'])
-def send_text(message):
-    if message.text.lower() == 'нам первый смайлик':
-        img = open('Смайлики и люди 1.png', 'rb')
-        
-        bot.send_document(message.chat.id, img) 
-    elif message.text.lower() == 'наш второй смайлик':
-        img = open('Смайлики и люди 2.png', 'rb')
-        bot.send_document(message.chat.id, img)    
-   
-    else:
-        bot.send_message(message.chat.id, 'Прости, но пока у меня нет этих Emoji')
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
-bot.polling()
-#сами смайлики прописать не могу, иначе публикация не обрабатывается на habr
+logger = logging.getLogger(__name__)
+TOKEN = '2059855205:AAEX2RnnkBQWnLHxw3PMx5JcYaZhGGvE_9E'
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+
+
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(TOKEN, use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://adult-system-control-bot.herokuapp.com//' + TOKEN)
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
