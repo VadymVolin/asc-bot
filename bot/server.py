6169f9a53767d0fe497afced5213ff91758e1339
Vadym Volin <vadym.volin@3ss.tv>
2021-12-18 14:02:29 +0200

    ASC: finished commit, link doesn't work properly

--- bot/server.py
+++ bot/server.py
@@ -10,6 +10,12 @@ API_KEY = os.getenv('API_KEY')
 bot = telebot.AsyncTeleBot(API_KEY)
 
 
+def notify_admins(chat_id, message):
+    admins = bot.get_chat_administrators(chat_id).wait()
+    for admin in admins:
+        bot.send_message(admin.user.id, message)
+
+
 @bot.message_handler(commands=['start'])
 def startCmd(message):
     start_learning()
@@ -27,13 +33,44 @@ def startCmd(message):
 
 @bot.message_handler(chat_types=['private'])
 def command_help(message):
-    message_text = message.text
-    print("Received private chat message: ", message_text)
-    result_text = "Received private chat message " \
-                  + message.from_user.first_name \
-                  + "(@" + message.from_user.username \
-                  + "): [" + message_text + "]. Thank you!"
-    bot.send_message(message.chat.id, result_text)
+    check_image_result = False
+    check_text_result = False
+    invalid_text_segment = ''
+
+    message_text = message.caption
+    message_photo = message.photo
+    message_sticker = message.sticker
+
+    if message_text:
+        check_text_result = check_text(message_text)
+
+    if check_text_result:
+        invalid_text_segment = "Text not valid"
+        bot.delete_message(message.chat.id, message.message_id)
+        result_text = "Received message with adult content from user: " + message.from_user.first_name + \
+            "(@" + message.from_user.username + \
+            "): [" + invalid_text_segment + \
+                      "]. We notified admin's of the current chat."
+        notify_admins(message.chat.id, result_text)
+
+    if (not check_text_result) and message_photo:
+        img_data = bot.get_file(message_photo[-1].file_id).wait()
+        img_url = BASE_IMAGE_URL.format(API_KEY, img_data.file_path)
+        check_image_result = check_image(img_url)
+
+    if (not check_text_result) and message_sticker:
+        img_data = bot.get_file(message_sticker.file_id).wait()
+        img_url = BASE_IMAGE_URL.format(API_KEY, img_data.file_path)
+        check_image_result = check_image(img_url)
+
+    if check_image_result:
+        bot.delete_message(message.chat.id, message.message_id)
+        invalid_text_segment = "Media not valid"
+        result_text = "Received message with adult content from user: " + message.from_user.first_name + \
+            "(@" + message.from_user.username + \
+            "): [" + invalid_text_segment + \
+                      "]. We notified admin's of the current chat."
+        notify_admins(message.chat.id, result_text)
 
 
 # Handle all sent documents of type 'text/plain'.
@@ -49,14 +86,20 @@ def command_help(message):
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def default_command(message):
+    check_text_result = False
+    invalid_text_segment = ''
     message_text = message.text
-    print("Received text message: ", message_text)
-    checker_result = " not " if check_text(message_text) else ""
-    result_text = "Received text message from " + message.from_user.first_name + \
-                  "(@" + message.from_user.username + \
-                  "): [" + message_text + \
-                  "]. This text is " + checker_result + "valid. Thank you!"
-    bot.send_message(message.chat.id, result_text)
+    if message_text:
+        check_text_result = check_text(message_text)
+
+    if check_text_result:
+        invalid_text_segment = "Text not valid"
+        bot.delete_message(message.chat.id, message.message_id)
+        result_text = "Received message with adult content from user: " + message.from_user.first_name + \
+            "(@" + message.from_user.username + \
+            "): [" + invalid_text_segment + \
+                      "]. We notified admin's of the current chat."
+        notify_admins(message.chat.id, result_text)
 
 
 # Handle all other messages.
@@ -78,15 +121,16 @@ def receive_img(message):
         invalid_text_segment = "Text not valid"
         bot.delete_message(message.chat.id, message.message_id)
         result_text = "Received message with adult content from user: " + message.from_user.first_name + \
-                  "(@" + message.from_user.username + \
-                  "): [" + invalid_text_segment + "]. We notified admin's of the current chat."
-        bot.send_message(message.chat.id, result_text)
+            "(@" + message.from_user.username + \
+            "): [" + invalid_text_segment + \
+                      "]. We notified admin's of the current chat."
+        notify_admins(message.chat.id, result_text)
 
     if (not check_text_result) and message_photo:
         img_data = bot.get_file(message_photo[-1].file_id).wait()
         img_url = BASE_IMAGE_URL.format(API_KEY, img_data.file_path)
         check_image_result = check_image(img_url)
-    
+
     if (not check_text_result) and message_sticker:
         img_data = bot.get_file(message_sticker.file_id).wait()
         img_url = BASE_IMAGE_URL.format(API_KEY, img_data.file_path)
@@ -96,10 +140,10 @@ def receive_img(message):
         bot.delete_message(message.chat.id, message.message_id)
         invalid_text_segment = "Media not valid"
         result_text = "Received message with adult content from user: " + message.from_user.first_name + \
-                  "(@" + message.from_user.username + \
-                  "): [" + invalid_text_segment + "]. We notified admin's of the current chat."
-        bot.send_message(message.chat.id, result_text)
-
+            "(@" + message.from_user.username + \
+            "): [" + invalid_text_segment + \
+                      "]. We notified admin's of the current chat."
+        notify_admins(message.chat.id, result_text)
 
 # @bot.message_handler(func=lambda message: True, content_types=['audio', 'photo', 'voice', 'video', 'document', 'text', 'location', 'contact', 'sticker'])
 # def default_command(message):
